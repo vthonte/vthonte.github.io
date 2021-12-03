2304d662f8a4ec75c8da9e9591250dbf4bdd8511
vthonte <vthonte@gmail.com>
2021-12-03 15:01:51 +0530

    Updates

---
+++ precache-manifest.5d634fa1cf113af186d9eab3e8202f59.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49404d2109149227300da79d6bb46b84",
+    "url": "/vthonte.github.io/index.html"
+  },
+  {
+    "revision": "9be6c7e77f225e234196",
+    "url": "/vthonte.github.io/static/css/main.0d5bd4a1.chunk.css"
+  },
+  {
+    "revision": "0472e6393639201babd5",
+    "url": "/vthonte.github.io/static/js/2.72828d6e.chunk.js"
+  },
+  {
+    "revision": "a80b1d0a0f2f6b78d2f3e95cd2a45711",
+    "url": "/vthonte.github.io/static/js/2.72828d6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9be6c7e77f225e234196",
+    "url": "/vthonte.github.io/static/js/main.217cf737.chunk.js"
+  },
+  {
+    "revision": "cafba2686adf1d4983f9",
+    "url": "/vthonte.github.io/static/js/runtime-main.63a808fb.js"
+  },
+  {
+    "revision": "aef7327c45c34da71fb879a282fc569a",
+    "url": "/vthonte.github.io/static/media/facebook.aef7327c.svg"
+  },
+  {
+    "revision": "874f98909122059c70b514a085dac955",
+    "url": "/vthonte.github.io/static/media/github.874f9890.svg"
+  },
+  {
+    "revision": "bb982d137f7aad3d9d64c3c556b792ff",
+    "url": "/vthonte.github.io/static/media/instagram.bb982d13.svg"
+  },
+  {
+    "revision": "67fdfcdc140b6b4a9229c48e905f954c",
+    "url": "/vthonte.github.io/static/media/linkedin-in.67fdfcdc.svg"
+  },
+  {
+    "revision": "05767691ba0292feb508e8050bd03bf4",
+    "url": "/vthonte.github.io/static/media/react.05767691.svg"
+  },
+  {
+    "revision": "5d0a2e9cfa1d86abf437fdca65cacae8",
+    "url": "/vthonte.github.io/static/media/twitter.5d0a2e9c.svg"
+  },
+  {
+    "revision": "f327023da0354faf54960aac4b1a8f4b",
+    "url": "/vthonte.github.io/static/media/whatsapp.f327023d.svg"
+  },
+  {
+    "revision": "285e53e00e2fa94944d7b58ed388bc42",
+    "url": "/vthonte.github.io/static/media/youtube.285e53e0.svg"
+  }
+]);
